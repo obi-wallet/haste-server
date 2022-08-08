63cc93bc4d628d8face930ba1f8cd988a2fa5739
Jonas Keinholz <git@inyono.dev>
2022-08-09 00:17:08 +0200

    generate digit keys

--- config.js
+++ config.js
@@ -1,16 +1,10 @@
 {
-
   "host": "0.0.0.0",
   "port": 7777,
-
   "keyLength": 10,
-
   "maxLength": 400000,
-
   "staticMaxAge": 86400,
-
   "recompressStaticAssets": true,
-
   "logging": [
     {
       "level": "verbose",
@@ -18,34 +12,30 @@
       "colorize": true
     }
   ],
-
   "keyGenerator": {
-    "type": "phonetic"
+    "type": "random",
+    "keyspace": "0123456789"
   },
-
   "rateLimits": {
     "categories": {
       "twilio": {
         "totalRequests": 500000,
-        "every":         60
+        "every": 60
       },
       "blacklist": {
         "totalRequests": 0,
-        "every":         0 
+        "every": 0
       },
       "normal": {
         "totalRequests": 60,
-        "every":         60
+        "every": 60
       }
     }
   },
-
   "storage": {
     "type": "file"
   },
-
   "documents": {
     "about": "./about.md"
   }
-
 }
